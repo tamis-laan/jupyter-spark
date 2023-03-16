e2322bc3e60703c70ba2b51e8fbb2637384bf913
tamis-laan <tamis@bisc.nl>
2023-03-16 18:00:01 +0100

    feat: print internal api server url

--- connect.sh
+++ connect.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+# Get internal API server link
+kubectl get endpoints kubernetes -n default
+
 # kubectl --namespace=jupyterhub port-forward service/proxy-public 8080:http
 kubectl port-forward service/proxy-public 8080:http
