7e1cf031bfcf92103262904cc33439287e27e15c
tamis-laan <tamis@bisc.nl>
2023-03-16 12:08:54 +0100

    feat: added test.sh that uses spark-submit

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from pyspark.sql import SparkSession
+
 spark = SparkSession.builder \
-    .master("k8s://https://127.0.0.1:39287") \
+    .master("k8s://https://127.0.0.1:8001") \
     .appName("Word Count") \
+    .config("spark.submit.deployMode","cluster") \
+    .config("spark.executor.instances", 5) \
     .config("spark.kubernetes.container.image", "gcr.io/spark/spark:v3.1.1") \
+    .config()
     .getOrCreate()
+
+df = spark.sql("select 'spark' as hello")
+
+df.show()
---
+++ test.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+					# kubectl cluster-info
+          # --master k8s://https://127.0.0.1:39287 \
+
+					# kubectl proxy
+          # --master k8s://https://127.0.0.1:8001 \
+
+# Run spark job as a test on kubernetes
+spark-submit \
+          --master k8s://https://127.0.0.1:39287 \
+          --deploy-mode cluster \
+          --name spark-pi \
+          --class org.apache.spark.examples.SparkPi \
+          --conf spark.executor.instances=5 \
+          --conf spark.kubernetes.container.image=gcr.io/spark-operator/spark-py:v3.1.1  \
+          local:///opt/spark/examples/src/main/python/pi.py
